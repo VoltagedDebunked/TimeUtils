19bc389d407384467055cb95d1d3f9cebbba3b04
Voltaged <140754373+VoltagedDebunked@users.noreply.github.com>
2024-07-17 13:03:39 -0500

    Create timeutils.h

---
+++ core/timeutils.h
@@ -0,0 +1,11 @@
+#ifndef TIME_UTILS_H
+#define TIME_UTILS_H
+
+#include <stdio.h>
+
+// Function declarations
+void print_current_datetime();
+void print_days_in_month(int month, int year);
+int is_leap_year(int year);
+
+#endif // TIME_UTILS_H
